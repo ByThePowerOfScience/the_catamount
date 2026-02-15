e13815a7a251346ad64c7cdad242b1ee7da9b64e
ByThePowerOfScience (ABadHaiku) <16433721+ByThePowerOfScience@users.noreply.github.com>
2026-02-14 22:01:30 -0500

    Update TheCatamountNeoForge.java

--- neoforge/src/main/java/dev/sterner/the_catamount/neoforge/TheCatamountNeoForge.java
+++ neoforge/src/main/java/dev/sterner/the_catamount/neoforge/TheCatamountNeoForge.java
@@ -146,7 +146,7 @@ public class TheCatamountNeoForge {
             float newAmount = ModEventHandlers.onLivingDamage(
                     event.getEntity(),
                     event.getSource(),
-                    event.getDamage()
+                    event.getNewDamage()
             );
             event.setNewDamage(newAmount);
         }
